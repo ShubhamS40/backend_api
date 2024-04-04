0b01e44c627375a4bc3a690b1f76cd5df928e442
shubham <shiva2004singh@gmail.com>
2024-04-04 23:10:01 +0530

    name add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000; // Use the PORT environment variable if a
 // Define your routes and middleware here
 
 app.get('/', (req, res) => {
-  res.send(`<img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-y5nQpJevw4sc7eQzGxwx0iwgMewk5B1rMg&s/> <h1>Backend project hello shubham</h1>`);
+  res.send(`<img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-y5nQpJevw4sc7eQzGxwx0iwgMewk5B1rMg&s/> <h1>Backend project hello mansi</h1>`);
 });
 
 // Start the server
